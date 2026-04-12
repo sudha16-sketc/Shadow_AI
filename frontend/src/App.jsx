d795e26ca15e8567197422713f818d2e5b37a8d3
Sudhakar Sutar <sudhakarsutar101@gmail.com>
2026-04-12 12:19:09 +0530

    all file setups done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <><h1>Shadow AI Auditor </h1>    </>
+    <>
+      <h1>Shadow_ai</h1>
+    </>
   )
 }
 
